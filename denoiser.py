7b65d6a28f59f972ba6c8e882db056a372514819
syjin311 <syjin311@yonsei.ac.kr>
2025-03-11 17:50:32 -0700

    leaving only hybrid filter version

--- denoiser.py
+++ denoiser.py
@@ -1,52 +1,127 @@
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
-from utils import normalize
-from regularizer import *
-import torch
-import torch.nn.functional as F
+import argparse
+from scipy.ndimage import uniform_filter
 DATA_PATH = "../dataset"
+from utils import normalize, rgb2gray, compute_gradients, mse, psnr
+
+def bilateral_get_weight(i_range, j_range, i, j, p, gradient_magnitudes, sigma_s, sigma_r, sigma_o):
+    patch = p[np.ix_(i_range, j_range)]
+    center_gradient = gradient_magnitudes[i, j]
+    neighbor_gradient = gradient_magnitudes[np.ix_(i_range, j_range)]
+    w = np.exp(
+        - ((patch - p[i, j])**2 / sigma_r**2)
+        - ((i_range[:, None] - i)**2 / sigma_s**2)
+        - ((j_range[None, :] - j)**2 / sigma_s**2)
+    )
+
+    # w_orientation = np.exp(
+    #     - ((neighbor_gradient[:,:,0] - center_gradient[0])**2 / sigma_o**2)
+    #     - ((neighbor_gradient[:,:,1] - center_gradient[1])**2 / sigma_o**2)
+    # )
+
+    orientation_distance = (np.linalg.norm(neighbor_gradient - center_gradient, axis=-1)**2)*np.linalg.norm(center_gradient)
+    w_orientation = np.exp(-orientation_distance/sigma_o**2)
+
+    # def softmax(x):
+    #     exp_x = np.exp(x - np.max(x))
+    #     return exp_x / exp_x.sum()
+    # neighbor_gradient /= (np.linalg.norm(neighbor_gradient, axis=-1, keepdims=True)+1e-6)
+    # w_orientation = neighbor_gradient.dot(center_gradient.reshape(-1, 1)).squeeze()
+    # w_orientation = softmax(w_orientation)
+
+    # dot_product = np.sum(neighbor_gradient * center_gradient, axis=-1)
+    # norms = np.linalg.norm(neighbor_gradient, axis=-1) * np.linalg.norm(center_gradient)
+    # orientation_similarity /= norms
+
+    return w, w_orientation, w*w_orientation
+
+def bilateral_filter(p, r, gradient_magnitudes, sigma_s, sigma_r, sigma_o):
+    H, W = p.shape
+    q = np.zeros((H, W))
+    for i in tqdm(range(H)):
+        for j in range(W):
+            i_range = np.arange(max(i - r, 0), min(i + r, H))
+            j_range = np.arange(max(j - r, 0), min(j + r, W))
+            patch = p[np.ix_(i_range, j_range)]            
+            w = bilateral_get_weight(i_range, j_range, i, j, p, gradient_magnitudes, sigma_s, sigma_r, sigma_o)[-1]
+
+            q[i, j] = np.sum(w * patch) / np.sum(w)
+    return q
+
+def adaptive_eps(variance, max_eps):
+    # normalized_variance = (variance - np.min(variance)) / (np.max(variance) - np.min(variance))
+    # adaptive_eps_map = max_eps-max_eps * normalized_variance
+
+
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+    # norm_var = (variance - np.mean(variance)) / (np.std(variance) + 1e-12)
+    # sigmoid_var = sigmoid(norm_var/t)
+    # adaptive_eps_map = max_eps * (1 - sigmoid_var) 
+    sigmoid_var = sigmoid((variance-0.010)/0.1)
+    adaptive_eps_map = max_eps * (1 - sigmoid_var)
+
+    # def softmax(x):
+    #     exp_x = np.exp(x - np.max(x))
+    #     return exp_x / exp_x.sum()
+    # norm_var = (variance - np.mean(variance)) / (np.std(variance) + 1e-12)
+    # softmax_var = softmax(norm_var.flatten()/t).reshape(variance.shape)
+    # adaptive_eps_map = max_eps * (1 - softmax_var) 
+    return adaptive_eps_map
+
+def guided_filter(I, p, r, eps):
+    mean_I = uniform_filter(I, size=r)
+    mean_p = uniform_filter(p, size=r)
+    
+    corr_I = uniform_filter(I * I, size=r)
+    corr_Ip = uniform_filter(I * p, size=r)
+    
+    var_I = corr_I - mean_I * mean_I
+    cov_Ip = corr_Ip - mean_I * mean_p
+
+    adaptive_eps_map = adaptive_eps(var_I, eps)
+    
+    a = cov_Ip / (var_I + adaptive_eps_map)
+    # a = cov_Ip / (var_I + eps)
+    b = mean_p - a * mean_I
+    
+    mean_a = uniform_filter(a, size=r)
+    mean_b = uniform_filter(b, size=r)
+    
+    q = mean_a * I + mean_b
+    return q
 
 
 if __name__ == "__main__":
+    # read arguments from command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_path", type=str)
+    parser.add_argument("--sigma_r", type=float, default=0.3)
+    parser.add_argument("--sigma_s", type=float, default=5)
+    parser.add_argument("--sigma_o", type=float, default=0.1)
+    parser.add_argument("--eps", type=float, default=2*1e-4)
+    args = parser.parse_args()
+
+    sigma_r = 0.3
+    sigma_s = 5
+    sigma_o = 0.1
+    eps = 2*1e-4
 
     image_id = 6
-    original = np.array(Image.open(f"{DATA_PATH}/original/{image_id:03d}.png"))[:, :, :3]
-    noised = np.array(Image.open(f"{DATA_PATH}/noised/{image_id:03d}.jpg"))
+    clean = np.array(Image.open(f"{DATA_PATH}/original/{image_id:03d}.png"))[:, :, :3]
+    noisy = np.array(Image.open(f"{DATA_PATH}/noised/{image_id:03d}.jpg"))
+    clean = rgb2gray(normalize(clean))
+    p = rgb2gray(normalize(noisy))
+    print(f'Before denoising MSE: {mse(clean, p)}, PSNR: {psnr(clean, p)}')
 
-    original = normalize(original)
-    noised = normalize(noised)
-    
-    iterations = 200
-    learning_rate = 1
-    alpha = 5
-    
-    original = torch.tensor(original, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0) # (1, 3, 600, 600)
-    noised = torch.tensor(noised, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0) # (1, 3, 600, 600)
-    denoised = noised.clone().detach().requires_grad_(True)
-    
-    print(f'initial difference: {F.mse_loss(denoised, original).item():.06f}')
+    magnitudes = compute_gradients(p, num_directions=2)
+    I = bilateral_filter(p, gradient_magnitudes=magnitudes, r=4,
+                        sigma_s=sigma_s, sigma_r=sigma_r, sigma_o=sigma_o)
+    q = guided_filter(I=I, p=p, r=3, eps=eps)
 
-    pbar = tqdm(range(iterations))
-    for i in pbar:
-        mse = F.mse_loss(denoised, noised)
-        
-        edge = laplacian3(denoised)
-        r1, r2, diff_sigmoid = differenceOfSigmoid(edge, c1=2, c2=10)
-        regularizer = 0 * diff_sigmoid + tv_norm(r2)
-        loss = mse + alpha * regularizer
-        
-        loss.backward()
+    print(f"After denoising MSE: {mse(clean, q)}, PSNR: {psnr(clean, q)}")
 
-
-        with torch.no_grad():
-            denoised -= learning_rate * denoised.grad
-            criteria = F.mse_loss(denoised, original)
-            pbar.set_description(f"Loss: {mse.item():.6f}+{regularizer:.6f}, criteria: {criteria.item():.6f}, grad: {denoised.grad.abs().mean().item():.6f}")
-
-        denoised.grad.zero_()
-
-    denoised_image = denoised.squeeze(0).permute(1,2,0).detach().numpy()
-    denoised_image = np.clip(denoised_image, 0, 1)
-    denoised_image = (denoised_image * 255).astype(np.uint8)
-    denoised_image = Image.fromarray(denoised_image).save(f"{DATA_PATH}/denoised/{image_id:03d}.png")
+    denoised = (np.clip(q, 0, 1)*255).astype(np.uint8)
+    denoised_image = Image.fromarray(denoised).save(f"{DATA_PATH}/denoised/{image_id:03d}.png")
--- utils.py
+++ utils.py
@@ -1,7 +1,90 @@
 import numpy as np
+from scipy.ndimage import sobel
 
 def normalize(image):
     if image.dtype == np.uint8:
         return image / 255
     else:
         return (image-image.min()) / (image.max()-image.min())
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+def compute_gradients(image, num_directions=2):
+    assert num_directions in [2, 4]
+    grad_x = sobel(image, axis=0)  # Gradient in x-direction
+    grad_y = sobel(image, axis=1)  # Gradient in y-direction
+
+    if num_directions==2:
+        return np.stack((np.abs(grad_x), np.abs(grad_y)), axis=-1)
+    else:
+        grad_45 = sobel(image, axis=-1)  # Gradient in 45-degree direction
+        grad_135 = sobel(image, axis=-2)  # Gradient in 135-degree direction
+        return np.stack((np.abs(grad_x), np.abs(grad_y), np.abs(grad_45), np.abs(grad_135)), axis=-1)
+
+def mse(image1, image2):
+    return np.mean((image1 - image2)**2)
+
+def psnr(clean, denoised):
+    mse_value = mse(clean, denoised)
+    if mse_value ==0:
+        return float("inf")
+    max_pixel = 1.0
+    psnr_value = 20 * np.log10(max_pixel / np.sqrt(mse_value))
+    return psnr_value
+
+def fill_foreground_patch(patch):
+    patch_flatten = patch.flatten()
+    var = np.var(patch_flatten)
+    _, (counts, bin_edges) = compute_entropy(patch_flatten, return_counts=True)
+    if var<0.01:
+        max_count_index = np.argmax(counts)
+        return bin_edges[max_count_index]
+    else:
+        return None
+
+def cluster_colors(image, num_clusters=5):
+    from sklearn.cluster import KMeans
+    H, W, C = image.shape
+    pixels = image.reshape(-1, 3)
+
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(pixels)
+    centers = kmeans.cluster_centers_
+    labels = kmeans.labels_
+
+    quantized_image = centers[labels].reshape(H, W, C)
+    if image.dtype == np.uint8:
+        return quantized_image.astype(np.uint8), centers.astype(np.uint8)
+    else:
+        return quantized_image, centers
+
+def compute_entropy(values, return_counts=False):
+    if values.dtype != np.int8:
+        values = (np.clip(values, 0, 1)*255).astype(np.uint8)
+    counts, bin_edges = np.histogram(values, bins=256, range=(0, 256))
+    
+    total_counts = len(values)
+    probabilities = counts / total_counts
+    
+    non_zero_probabilities = probabilities[probabilities > 0]
+    entropy = -np.sum(non_zero_probabilities * np.log(non_zero_probabilities))
+    
+    if return_counts:
+        return entropy, (counts, bin_edges)
+    else:
+        return entropy
+
+def remove_block_artifact(image):
+    H, W = image.shape
+    patch_size = 8
+    stride = 1
+
+    mask = image.copy()
+    for i in range(0, H-patch_size, stride):
+        for j in range(0, W-patch_size, stride):
+            patch = image[i:i+patch_size, j:j+patch_size]
+            if np.var(patch.flatten()) < 0.01:
+                fill_value = fill_foreground_patch(patch)/255
+                if fill_value is not None:
+                    mask[i][j] = fill_value
+    return mask
